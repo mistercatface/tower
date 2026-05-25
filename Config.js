7d3b8d7fb250e79ba569891f185b6ec7c9956344
mistercatface <mistercatface@proton.me>
2026-05-25 10:41:55 -0600

    adjustment

--- Config.js
+++ Config.js
@@ -16,7 +16,7 @@ export const enemyTypes = [
     },
     { type: "standard", weight: 60, radius: 6, baseSpeed: 50, baseHealth: 1, maxHealth: 500, color: "#F44336", minLevel: 0, spawnType: "single", attackType: "ranged", canDodge: false },
     { type: "fast", weight: 5, radius: 5, baseSpeed: 70, baseHealth: 0.5, maxHealth: 300, color: "#FFEB3B", minLevel: 2, spawnType: "single", attackType: "ranged", canDodge: false },
-    { type: "spastic", weight: 15, radius: 4, baseSpeed: 130, baseHealth: 0.3, maxHealth: 50, color: "#E91E63", minLevel: 3, spawnType: "group", attackType: "charge", canDodge: false, accelRate: 0.35, canDamageWalls: true, groupSettings: { baseGroupSize: 5, growthPerWave: 2.0 } },
+    { type: "spastic", weight: 15, radius: 4, baseSpeed: 100, baseHealth: 0.3, maxHealth: 50, color: "#E91E63", minLevel: 3, spawnType: "group", attackType: "charge", canDodge: false, accelRate: 0.35, canDamageWalls: true, groupSettings: { baseGroupSize: 5, growthPerWave: 2.0 } },
     { type: "tank", weight: 10, radius: 8, baseSpeed: 33, baseHealth: 3, maxHealth: 1200, color: "#9C27B0", minLevel: 0, spawnType: "single", attackType: "ranged", canDodge: false },
     { type: "dodger", weight: 20, radius: 6, baseSpeed: 50, baseHealth: 1, maxHealth: 450, color: "#03A9F4", minLevel: 3, spawnType: "single", attackType: "ranged", canDodge: true },
     { type: "boss", weight: 0, radius: 8, baseSpeed: 20, baseHealth: 50, maxHealth: 15000, color: "#B71C1C", minLevel: 0, spawnType: "single", attackType: "ranged", canDodge: false },
