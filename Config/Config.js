e7a8c6fd05d95da97c732fcb7ea666835ded351a
mistercatface <mistercatface@proton.me>
2026-05-31 22:43:45 -0600

    yes

--- Config/Config.js
+++ Config/Config.js
@@ -371,16 +371,16 @@ export const explosionSettings = { defaultDamage: 5, barrelDamage: 5, wallBlastD
 export const enemyBaseStats = { turnSpeed: 10, range: 112, accuracy: 0.9, penetration: 0, moveSpeedMultiplier: 1.0, fireIntervalMultiplier: 1.0, reloadSpeedMultiplier: 1.0, speed: 100 };
 
 export const THEME_COLORS = [
-    { r: 0, g: 188, b: 212 },
-    { r: 76, g: 175, b: 80 },
-    { r: 255, g: 152, b: 0 },
-    { r: 156, g: 39, b: 176 },
-    { r: 63, g: 81, b: 181 },
-    { r: 244, g: 67, b: 54 },
-    { r: 233, g: 30, b: 99 },
-    { r: 0, g: 150, b: 136 },
-    { r: 205, g: 220, b: 57 },
-    { r: 121, g: 85, b: 72 },
+    { r: 240, g: 240, b: 242 }, // Bright Industrial White
+    { r: 112, g: 128, b: 144 }, // Slate Gray
+    { r: 195, g: 195, b: 190 }, // Light Concrete / Cement
+    { r: 64, g: 74, b: 84 },    // Dark Slate / Charcoal
+    { r: 130, g: 145, b: 160 }, // Steel Blue / Muted Iron
+    { r: 150, g: 150, b: 145 }, // Rough Concrete
+    { r: 225, g: 228, b: 230 }, // Cool Off-White
+    { r: 170, g: 180, b: 188 }, // Ashes / Light Slate
+    { r: 90, g: 90, b: 88 },    // Dark Concrete / Asphalt
+    { r: 210, g: 215, b: 218 }, // Silvery Steel
 ];
 
 export const controlSettings = { doubleTapTimeout: 300, scrollZoomSensitivity: -0.001 };
