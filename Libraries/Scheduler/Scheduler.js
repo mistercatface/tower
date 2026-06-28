52b30e58a8af315f4f851b770028c9e2b878f4a8
mistercatface <mistercatface@proton.me>
2026-06-28 17:30:01 -0600

    sure

--- Libraries/Scheduler/Scheduler.js
+++ Libraries/Scheduler/Scheduler.js
@@ -5,13 +5,13 @@ export class Scheduler {
     }
     schedule(delay, callback = null, isLoop = false) {
         const id = this.nextId++;
-        const event = { id, timer: delay, delay, callback, isLoop, remove: false, paused: false };
+        const safeDelay = delay > 0 ? delay : 1;
+        const event = { id, timer: safeDelay, delay: safeDelay, callback, isLoop, paused: false };
         this.events.set(id, event);
         return id;
     }
     cancel(id) {
-        const event = this.events.get(id);
-        if (event) event.remove = true;
+        this.events.delete(id);
     }
     pause(id) {
         const event = this.events.get(id);
@@ -23,28 +23,24 @@ export class Scheduler {
     }
     getTimeRemaining(id) {
         const event = this.events.get(id);
-        return event && !event.remove ? event.timer : 0;
+        return event ? event.timer : 0;
     }
     clear() {
         this.events.clear();
     }
     update(dt) {
         for (const [id, event] of this.events) {
-            if (event.remove) {
-                this.events.delete(id);
-                continue;
-            }
             if (event.paused) continue;
             event.timer -= dt;
             if (event.timer <= 0)
                 if (event.isLoop)
-                    while (event.timer <= 0) {
+                    while (event.timer <= 0 && this.events.has(id)) {
                         if (event.callback) event.callback();
                         event.timer += event.delay;
                     }
                 else {
                     if (event.callback) event.callback();
-                    event.remove = true;
+                    this.events.delete(id);
                 }
         }
     }
